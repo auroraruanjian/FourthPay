b6ead075ac51932c58cd8db61e243fd2a90c5589
Nick <nick_it_038@ie8.com>
2019-06-07 19:59:20 +0800

    vue 路由权限

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,16 +1,18 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import NProgress from 'nprogress' // progress bar
 import store from '@/store';
 import {Message} from 'element-ui';
 
-import index from '@/views/default/index';
+// import index from '@/views/default/index';
 
 Vue.use(VueRouter);
+NProgress.configure({ showSpinner: false }) // NProgress Configuration
 
 /* Layout */
 import Layout from '@/layout'
 
-const routes = [
+export const constantRoutes = [
     {
         path: '/login',
         component : () => import('@/views/users/login'),
@@ -31,25 +33,34 @@ const routes = [
     },
 ]
 
+export const asyncRoutes = [
+
+]
+
 const router = new VueRouter({
-    routes,
+    routes: constantRoutes,
 });
 
 router.beforeEach((to, from, next) => {
+    // start progress bar
+    NProgress.start()
+
     let token = JSON.parse(window.localStorage.getItem('userInfo'));
-    if (to.path === '/login') {
-        if(store.state.user.username){
-            Message.warning("您已经登录，请先退出登录!");
+    if( token ){
+        if (to.path === '/login') {
             next({path:'/'});
-        }else {
-            window.localStorage.removeItem('userInfo');
-            next();
+            NProgress.done();
+        }else{
+            // 权限检查注册
+            next()
         }
-    } else if (!token && to.path != '/login'){
-        next({ path: '/login' });
-    } else {
-        next();
+    }else{
+        next(`/login?redirect=${to.path}`)
+        NProgress.done()
     }
+
+
+    NProgress.done()
 });
 
 export default router
--- resources/js/store/getters.js
+++ resources/js/store/getters.js
@@ -2,6 +2,9 @@ const getters = {
     username: state => state.user.username,
     sidebar: state => state.app.sidebar,
     avatar: state => state.user.avatar,
+    visitedViews: state => state.tagsView.visitedViews,
+    cachedViews: state => state.tagsView.cachedViews,
+    permission_routes: state => state.permission.routes,
 }
 
 export default getters
